97d16dd8dbf91f3d3c0cd4e0143b95bc741948ef
Nahuel Ezequiel Mendoza <ezequielnahuel11@gmail.com>
2025-09-22 22:25:24 -0300

    NUEVO4

--- javaScript/menuHAmburguesa.js
+++ javaScript/menuHAmburguesa.js
@@ -1,7 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-links > li > a');
 
-hamburger.addEventListener('click', () => { 
+if (hamburger && navLinks) {
+  hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     hamburger.classList.toggle('open');
+  });
+}
+
+// Para abrir submenús en móvil
+menuItems.forEach(item => {
+  item.addEventListener('click', (e) => {
+    const parentLi = item.parentElement;
+    if (parentLi.querySelector('ul')) {
+      e.preventDefault(); // Evita que se vaya a la página
+      parentLi.classList.toggle('open');
+    }
+  });
 });
