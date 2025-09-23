d87154f5ac4ce296c1b6899315cc848c1d5f046e
Nahuel Ezequiel Mendoza <ezequielnahuel11@gmail.com>
2025-09-22 21:56:21 -0300

    NUEVO2

--- javaScript/menuHAmburguesa.js
+++ javaScript/menuHAmburguesa.js
@@ -1,5 +1,5 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener('click', () => { 
     navLinks.classList.toggle('active');
