954a6928cc5ec3ee0029edc434bbed2b83884e99
Nahuel Ezequiel Mendoza <ezequielnahuel11@gmail.com>
2025-09-24 19:27:27 -0300

    seguimos modificando el menu otra vezx4

--- javaScript/menuHAmburguesa.js
+++ javaScript/menuHAmburguesa.js
@@ -1,19 +1,18 @@
-  document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector (".hamburger");
-  const navLinks = document.querySelector (".nav-links");
-  const serviciosItem = document.querySelector (".nav-links li:nth-child(2) > a");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const serviciosItem = document.querySelector(".nav-links li:nth-child(2) > a");
   const serviciosLi = serviciosItem.parentElement;
 
   // Toggle menú hamburguesa
   hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+    navLinks.classList.toggle("active");
   });
 
   // Toggle submenú en "Servicios"
   serviciosItem.addEventListener("click", (e) => {
-    // Solo en móvil
-   if (window.innerWidth <= 768) {
-      e.preventDefault(); // Evita que navegue
+    if (window.innerWidth <= 768) {
+      e.preventDefault(); // Evita que navegue directo
       serviciosLi.classList.toggle("open");
     }
   });
